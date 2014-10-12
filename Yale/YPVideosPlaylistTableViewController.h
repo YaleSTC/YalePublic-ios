0f837811ba0baf9cc4ec62b241055ffff3731bca
Minh Tri Pham <minhtri@pham.cz>
2014-10-12 13:36:39 -0400

    set up

---
+++ Yale/YPVideosPlaylistTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YPVideosPlaylistTableViewController.h
+//  Yale
+//
+//  Created by Minh Tri Pham on 10/12/14.
+//  Copyright (c) 2014 Hengchu Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YPVideosPlaylistTableViewController : UITableViewController
+
+@end
