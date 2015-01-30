007809393c2df8890704fbb6975db824e5d7e58d
Minh Tri Pham <minhtri@pham.cz>
2015-01-30 14:45:16 -0500

    removed searchBars

--- Yale/YPNewsTopicsTableViewController.h
+++ Yale/YPNewsTopicsTableViewController.h
@@ -9,6 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface YPNewsTopicsTableViewController : UITableViewController
-@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
--- Yale/YPVideosPlaylistTableViewController.h
+++ Yale/YPVideosPlaylistTableViewController.h
@@ -9,6 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface YPVideosPlaylistTableViewController : UITableViewController
-@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
